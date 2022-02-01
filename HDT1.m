02be0a17a1228f8a76f4db3a64a77dd7fa7129ab
gil19443 <68124154+gil19443@users.noreply.github.com>
2022-01-31 22:38:32 -0600

    ultimos cambios

--- HDT1.m
+++ HDT1.m
@@ -7,24 +7,19 @@ syms x;
 %f = (x^(2) + 1)/3;
 f = 3*x*(1-x);
 fx = inline(f);
-%aux = feval(fx,xi);
 x0=0.3;
 error = 100;
 i=0;
 fprintf('\ti  \txi  \terror \n');
 while (abs(error)>0.0000001)
-%for i=1:3
     i = i+1;
     xi= feval(fx,x0);
     error = xi - x0;
-    %error=((xi-x0)/xi)*100;
     fprintf('\t%d  \t%f  \t%f \n',i,xi,abs(error))
     x0=xi;
     
 end
 %% metodo de NR
-% Author - Ambarish Prashant Chandurkar
-%The Newton Raphson Method
 clc;
 close all;
 clear all;
@@ -42,27 +37,31 @@ x0 = input('Enter the intial approximation:');
 i = 0;
 error = 1;
 fprintf('\ti  \txi  \terror \n');
-% fplot(tanh(x),[-10,10]);
-hold on;
 % while(abs(error)>0.0000001)
-    for c = 1:6
-    i = i + 1;
-    f0=feval(fx,x0); %Calculating the value of function at x0
-    f0_der=feval(gx, x0); %Calculating the value of function derivative at x0
-    y=x0-f0/f0_der; % The Formula
-    error=abs(y-x0);
-    x0 = y;
-    fprintf('\t%d  \t%f  \t%f \n',i,y,error)
-    figure(1);
-    x1 = -5e5:0.5:5e5; 
-    plot(x1,tanh(x1),'r');
-    yt = ((sech(x0))^2*(x1-x0))+tanh(x0);
-    plot(x1,yt,'b');
-    plot(x0, tanh(x0),'*');
-    hold on;
-    xlabel("eje x");
-    ylabel("eje y");
-    title('tanh(xi) con x0 = 1.09');
+    c=0;
+    for c = 1:9
+        i = i + 1;
+        f0=feval(fx,x0); %Calculating the value of function at x0
+        f0_der=feval(gx, x0); %Calculating the value of function derivative at x0
+        y=x0-f0/f0_der; % The Formula
+        error=abs(y-x0);
+        x0 = y;
+        fprintf('\t%d  \t%f  \t%f \n',i,y,error)
+        try 
+            figure(1);
+            x1 = linspace(-14,14,100); 
+            plot(x1,tanh(x1),'r');
+            yt = ((sech(x0))^2*(x1-x0))+tanh(x0);
+            plot(x1,yt,'b');
+            plot(x0, tanh(x0),'*');
+            hold on;
+            xlabel("eje x");
+            ylabel("eje y");
+            ylim([-3 3]);
+            title('tanh(xi) con x0 = 1.09');
+        catch 
+            fprintf('el metodo murio');
+        end
     end
 % end
 %% metodo de la secante 
